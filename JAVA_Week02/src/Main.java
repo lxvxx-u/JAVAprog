94e103190fa6b3aedaee453c3987f1c1ef4b3a5c
Administrator <Administrator@172.17.77.62>
2024-09-30 20:28:52 +0900

    20:28

--- JAVA_Week02/src/Main.java
+++ JAVA_Week02/src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
-	
-	
-	
+	public static void main(String[] args) {
+		
+		// Scanner sc = new Scanner(System.in);
+		// 섭씨 -> 화씨
+		// 24.3도
+		float celsius = 24.3f;
+		float fahrenheit = celsius * (9/5) + 32;
+		System.out.printf("Fahrenheit %.2f is %f in Celsius\n", fahrenheit, celsius);
+		
+	}
+
 }
