8433c47f84e4c584a159c5dab42650a8e4919157
lxvxx-u <unstand0624@gmail.com>
2024-10-07 18:32:45 +0900

    Update 01.java

--- assignment/01.java
+++ assignment/01.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
+        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
 
         // dBm(데시벨 밀리와트) 계산하기
         // W(전력) 게산하기
@@ -9,6 +13,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         double dbm, V, R, P;
+
 
         System.out.print("V(전압)을 입력하세요.\n입력: ");
         V = scan.nextDouble();
@@ -18,10 +23,10 @@ public class Main {
 
         // 전력
         P = V * V / R;
-
+        
         // dBm
-        dBm = 10 * Math.log10(P) + 30;
+        dbm = 10 * Math.log10(P) + 30;
 
-        System.out.println("전력 : " + P + "\ndBm : " + dBm );
+        System.out.println("전력 : " + P + "\ndBm : " + dbm );
         }
     }
