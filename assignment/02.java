932bc969a14d3b2ec6edaacae6a975a6e858c1b9
lxvxx-u <unstand0624@gmail.com>
2024-10-14 17:38:13 +0900

    Create 02.java

---
+++ assignment/02.java
@@ -0,0 +1,70 @@
+
+class Car{
+    private int gas;
+    private String carName;
+
+    Car() { }
+    Car(String carName){
+        this.carName = carName;
+    }
+    Car(int gas){
+        this.gas = gas;
+    }
+    Car(String carName, int gas){
+        this.carName = carName;
+        this.gas = gas
+    }
+    public void setGas(int gas){
+        this.gas = gas;
+    }
+    public int getGas() {
+        return gas;
+    }
+    public void setCarName(String carName){
+        this.carName = carName;
+    }
+    public String getCarName(){
+        return carName;
+    }
+    public int move(int gas){
+        if(this.gas >= gas){
+            this.gas -= gas;
+            return this.gas;
+        }
+        else
+            return this.gas - gas;
+    }
+    public int refuel(int amount){
+        gas += amount;
+        return gas;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        Car[] cars = new Car[4];
+        String[] carNames = {"kia", "ferrari", "bmw", "toyota"};
+        int[] carGas = {10, 20, 15, 17};
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(carNames[i], carGas[i]);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            for (Car a : cars) {
+                if (a.getGas >= 5) {
+                    a.move(5);
+                    a.refuel(10);
+                }
+                System.out.println();
+            }
+
+
+
+
+
+        }
+    }
+}
+
